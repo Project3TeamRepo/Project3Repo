0170a61985813a382141a879173298fa0b7f20f0
Mikaela Mckee <mikaelamckee@iMac.local>
2018-11-07 19:37:37 -0600

    reactcalendar

---
+++ public/js/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import reactCalendar from "./styles/reactCalendar";
+
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div id="logo">
+            <span className="icon">date_range</span>
+            <span>
+              react<b>calendar</b>
+            </span>
+          </div>
+        </header>
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
