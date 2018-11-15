026a4a53543b57023497c3586692a10a8682dd7d
Mikaela McKee <mikaelamckee2010@yahoo.com>
2018-11-15 16:06:00 -0600

    merging files2

---
+++ MikaelaThree/reactcalendar/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import ReactCalendar from "./reactCalendar";
+
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div id="logo">
+            <span className="icon">date_range</span>
+            <span>
+              Ultimate<b>Organizer</b>
+            </span>
+          </div>
+        </header>
+        <main>
+          <ReactCalendar />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
